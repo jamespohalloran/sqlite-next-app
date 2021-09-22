8b62c552f7d7fd7b9ebe77dc59953dcd23a6f52f
James O'Halloran <james.p.ohalloran@gmail.com>
2021-09-22 08:17:41 -0300

    fix broken path

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import sqlite from "sqlite3";
 
 const relPath = process.env.NODE_ENV == 'development' ? `${process.cwd()}/public` : process.cwd()
-const db = new sqlite.Database(`${relPath}/public/sqlite.db`);
+const db = new sqlite.Database(`${relPath}/sqlite.db`);
 
 export default db;
